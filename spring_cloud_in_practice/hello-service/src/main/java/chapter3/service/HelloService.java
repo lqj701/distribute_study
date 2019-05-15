3f4c75ddf37de3477f1debe1b68b12358ff4c0c3
李强进 <li.qj@ikcrm.com>
2019-05-15 10:40:19 +0800

    修改

---
+++ spring_cloud_in_practice/hello-service/src/main/java/chapter3/service/HelloService.java
@@ -0,0 +1,19 @@
+package chapter3.service;
+
+import org.springframework.web.bind.annotation.*;
+
+import chapter3.web.User;
+
+@RequestMapping("/refactor")
+public interface HelloService {
+
+    @RequestMapping(value = "/hello4", method = RequestMethod.GET)
+    String hello(@RequestParam("name") String name);
+
+    @RequestMapping(value = "/hello5", method = RequestMethod.GET)
+    User hello(@RequestHeader("name") String name, @RequestHeader("age") Integer age);
+
+    @RequestMapping(value = "/hello6", method = RequestMethod.POST)
+    String hello(@RequestBody User user);
+
+}
